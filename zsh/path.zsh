ab6e5886e8bca8c8a199b6c4c64b16e51b03d13f
rvalente <rawn027@gmail.com>
2013-03-18 19:33:33 -0400

    added delivery to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/.deliver/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
