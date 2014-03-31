7c9fd8f760db8ebd9a2593783e9fbd09f61645c4
rvalente <rawn027@gmail.com>
2014-03-31 10:49:13 -0400

    added go and gopath

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,3 @@
-export PATH="$HOME/.deliver/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
