3f86779ebf5cec8e04e0587e2309fb62f3e81501
Ronald Valente <rawn027@gmail.com>
2014-05-28 08:57:37 -0400

    updated with awesome vagrant script

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
 export GOPATH="$HOME/go"
-export PATH="$GOPATH/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$GOPATH/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/usr/local/opt/go/libexec/bin"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
