2e940988b9567df77a9bc8b3e0ba7b33cdef4ac8
rvalente <rawn027@gmail.com>
2014-07-03 10:44:50 -0400

    added ra aliases for razor

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -51,3 +51,6 @@ alias cl='clear'
 # Sublime Text
 alias s3='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 alias s2='/Applications/Sublime\ Text 2.app/Contents/SharedSupport/bin/subl'
+
+# Razor Aliases
+alias ra='razor -u calo-rtp-razor:8080/api'
