d9eabb52274fe16cc176b2d46b6d8e0919e626fd
Ronald Valente <rawn027@gmail.com>
2014-10-01 20:30:54 -0400

    added docker and boot2docker as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 #
 DOTFILES="$HOME/.dotfiles"
 BREW='/usr/local/bin/brew'
-PKGS=( hg ansible ctags spark asciidoc graphviz fop wget fping nmap vim jshon rbenv ruby-build git zsh ag sysdig tmux reattach-to-user-namespace )
+PKGS=( hg ansible docker boot2docker ctags spark asciidoc graphviz fop wget fping nmap vim jshon rbenv ruby-build git zsh ag sysdig tmux reattach-to-user-namespace )
 BUNDLE_DIR="$HOME/.vim/bundle"
 
 #
