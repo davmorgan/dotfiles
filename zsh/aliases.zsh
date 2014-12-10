c8fbeed8f994a09f4ade9eef592fbe77faf08f7d
Ronald Valente <rawn027@gmail.com>
2014-12-10 13:52:33 -0500

    updated to use rcm instead of bootstrap...

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -60,3 +60,24 @@ alias av='ansible-vault edit'
 
 # Homebrew
 alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
+
+# Git
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias ga='git add .'
+alias gall='git add -A'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gcv='git commit -v -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gitk="/usr/bin/wish $(which gitk)"
+
+# Ruby
+alias b='bundle exec'
+alias bi='bindle install'
+alias bu='bundle update'
